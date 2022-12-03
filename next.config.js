b5af47e99711c3d2c4f6f097d7950f844d0b7346
ycjcl868 <chaolinjin@gmail.com>
2022-12-03 13:04:47 +0800

    chore: preact

--- next.config.js
+++ next.config.js
@@ -4,16 +4,4 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 module.exports = withBundleAnalyzer({
   swcMinify: true,
-  webpack(config, { dev, isServer }) {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-
-    return config;
-  },
 });
