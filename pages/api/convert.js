751b75ddcfefe0ccaecc9e4efd43158f93b4d584
ycjcl868 <chaolinjin@gmail.com>
2024-03-06 11:02:32 +0800

    chore: convert

--- pages/api/convert.js
+++ pages/api/convert.js
@@ -18,7 +18,7 @@ module.exports = async (req, res) => {
       url,
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+          "ClashX Pro/1.72.0.4 (com.west2online.ClashXPro; build:1.72.0.4; macOS 12.0.1) Alamofire/5.4.4",
       },
     });
     configFile = result.data;
